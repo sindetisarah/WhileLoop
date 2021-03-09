5511e0788e8d1bb15c13ad2d8d98a5ab27c11f83
sindetisarah <sindetisara@gmail.com>
2021-03-09 08:49:15 +0300

    first commit

---
+++ node.js
@@ -0,0 +1,18 @@
+// while loop condition in an array
+function fruits (){
+    /*list of the fruits*/
+    fruits =["Avacado","grapes","mangoes","orange","kiwi"] 
+//    returns the each fruits as per their index in decrement format 
+    
+let n =fruits.length;
+    while(n>-1){
+        n--
+        console.log(fruits[n]);
+    }
+}
+fruits()
+// kiwi
+// orange
+// mangoes
+// grapes
+// Avacado (expected output)
